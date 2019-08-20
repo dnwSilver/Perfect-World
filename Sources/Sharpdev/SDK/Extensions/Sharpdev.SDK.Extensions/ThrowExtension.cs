5dac5d46f6ddaf9acaea332f0e54513117539c4c
dnwSilver <dnwSilver@gmail.com>
2019-08-20 17:56:12 +0500

    Обновил тесты для Yield.

--- Sources/Sharpdev/SDK/Extensions/Sharpdev.SDK.Extensions/ThrowExtension.cs
+++ Sources/Sharpdev/SDK/Extensions/Sharpdev.SDK.Extensions/ThrowExtension.cs
@@ -3,32 +3,11 @@ using System.Text;
 
 namespace Sharpdev.SDK.Extensions
 {
+    /// <summary>
+    ///     Набор расширений для работы с ошибками.
+    /// </summary>
     public static class ThrowExtension
     {
-        /// <summary>
-        ///     Вызывает исключение TException, если выполняется условие.
-        /// </summary>
-        /// <typeparam name="TException">Тип исключения.</typeparam>
-        /// <param name="condition">Условие для вызова исключения.</param>
-        /// <param name="message">Сообщение.</param>
-        public static void If<TException>(bool condition, string message)
-            where TException : Exception
-        {
-            if (condition)
-                throw New.Instance<TException, string>(message);
-        }
-
-        /// <summary>
-        ///     Вызывает исключение <see cref="ArgumentNullException" />, если параметр не указан.
-        /// </summary>
-        /// <param name="param">Параметр.</param>
-        /// <param name="paramName">Название параметра.</param>
-        public static void IfNull(object param, string paramName)
-        {
-            if (param == null)
-                throw New.Instance<ArgumentNullException, string>(paramName);
-        }
-
         /// <summary>
         ///     Преобразование исключения с его внутренними исключениями в строку.
         /// </summary>
@@ -49,37 +28,6 @@ namespace Sharpdev.SDK.Extensions
             }
 
             return sb.ToString();
-        }
-
-        /// <summary>
-        ///     Пытается найти исключение определенного типа при помощи рекурсии.
-        /// </summary>
-        /// <typeparam name="T">Тип искомого исключения.</typeparam>
-        /// <param name="ex">Исходное исключение.</param>
-        /// <returns></returns>
-        public static T FindException<T>(this Exception ex)
-        {
-            while(true)
-            {
-                if (ex is T)
-                    return (T)(object)ex;
-
-                var inner = ex?.InnerException;
-
-                switch(inner)
-                {
-                    case null:
-
-                        return default;
-                    case T _:
-
-                        return (T)(object)inner;
-                    default:
-                        ex = inner;
-
-                        break;
-                }
-            }
         }
     }
 }
