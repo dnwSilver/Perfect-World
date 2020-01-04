07c20d4a024dec547234acb73735c36865454010
dnwSilver <dnwSilver@gmail.com>
2020-01-04 11:19:30 +0500

    Additional editorconfig

--- Sources/Sharpdev/SDK/DDD/Sharpdev.SDK.DDD/Domain/Factories/EntityFactoryBase.cs
+++ Sources/Sharpdev/SDK/DDD/Sharpdev.SDK.DDD/Domain/Factories/EntityFactoryBase.cs
@@ -29,7 +29,7 @@ namespace Sharpdev.SDK.Domain.Factories
         /// </remarks>
         public Result<TEntity> Create(IEntityBuilder<TEntity> entityToCreate)
         {
-            entityToCreate.Identifier = Identifier<TEntity>.New(default);
+            entityToCreate.Identifier = Identifier<TEntity>.New();
 
             return this.Recovery(entityToCreate);
         }
