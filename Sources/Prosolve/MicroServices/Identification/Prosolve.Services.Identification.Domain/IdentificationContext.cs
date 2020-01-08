5610b7c10225c46ab73c5f7214409f6bf487bc96
dnwSilver <dnwSilver@gmail.com>
2020-01-08 21:12:42 +0500

    Устранил ещё пару технических долгов.

--- Sources/Prosolve/MicroServices/Identification/Prosolve.Services.Identification.Domain/IdentificationContext.cs
+++ Sources/Prosolve/MicroServices/Identification/Prosolve.Services.Identification.Domain/IdentificationContext.cs
@@ -6,9 +6,9 @@ using Sharpdev.SDK.Domain;
 
 namespace Prosolve.Services.Identification
 {
-    internal class IdentificationContext: DbContext, IBoundedContext
+    public class IdentificationContext: DbContext, IBoundedContext
     {
-        public IdentificationContext(DbContextOptions options)
+        public IdentificationContext(DbContextOptions<IdentificationContext> options)
             : base(options)
         {
             
@@ -18,18 +18,16 @@ namespace Prosolve.Services.Identification
         protected override void OnModelCreating(ModelBuilder builder)
         {
             // todo Как-то тут всё подозрительно работает, такое ощущение что оно НЕ РАБОТАЕТ
-            // https://entityframeworkcore.com/knowledge-base/49526370/is-there-a-data-annotation-for-unique-constraint-in-ef-core--code-first--
-            
             builder.Entity<UserDataModel>().HasIndex(u => u.EmailAddress).IsUnique();
             builder.Entity<UserDataModel>().HasIndex(u => u.PhoneNumber).IsUnique();
         }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 // todo Вынести в файлы настроек строку подключения к БД.
-                optionsBuilder.UseNpgsql(
-                    @"port=5432 dbname=watcher user=watcher password=watcher_password");
+                optionsBuilder.UseNpgsql(@"port=5432 dbname=watcher user=watcher password=watcher_password");
             }
         }
     }
